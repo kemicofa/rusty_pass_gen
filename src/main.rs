a6b34528a572ae3eb3c5ad3991d916ae58b71aa6
kevinf <45143945+kemicofa@users.noreply.github.com>
2020-08-09 15:35:27 +0200

    simplified matches

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rand::random;
-use clap::{App, load_yaml};
+use clap::{App, load_yaml, ArgMatches};
 use std::cmp::Ordering;
 const LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const NUMBERS: &str = "1234567890";
@@ -30,15 +30,19 @@ fn shuffle(string: String) -> String {
     shuffed_vec.into_iter().collect()
 }
 
+fn parse_input <T: std::str::FromStr>(matches: &ArgMatches, name: &str) -> Option<T> {
+    matches.value_of(name)?.parse().ok()
+}
+
 fn main() {
 
     let yaml = load_yaml!("cli.yml");
     let matches = App::from(yaml).get_matches();
 
-    let password_length: f32 = matches.value_of("length").unwrap_or("20").parse().unwrap();
-    let numbers_percent: f32 = matches.value_of("numbers").unwrap_or("0.25").parse().unwrap();
-    let symbols_percent: f32 = matches.value_of("symbols").unwrap_or("0.25").parse().unwrap();
-    let letters_percent: f32 = matches.value_of("letters").unwrap_or("0.5").parse().unwrap();
+    let password_length: f32 = parse_input(&matches, "length").unwrap_or(20.);
+    let numbers_percent: f32 = parse_input(&matches, "numbers").unwrap_or(0.25);
+    let symbols_percent: f32 = parse_input(&matches, "symbols").unwrap_or(0.25);
+    let letters_percent: f32 = parse_input(&matches, "letters").unwrap_or(0.5);
 
     if numbers_percent + symbols_percent + letters_percent > 1.0 {
         panic!("number of symbols + numeric values + letters cannot exceed 100%");
