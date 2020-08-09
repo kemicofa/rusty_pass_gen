5137a3b56b0060b04b6e34c41f69ebe420875672
kevinf <45143945+kemicofa@users.noreply.github.com>
2020-08-09 15:03:39 +0200

    shuffled password

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
 use rand::random;
 use clap::{App, load_yaml};
+use std::cmp::Ordering;
 const LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const NUMBERS: &str = "1234567890";
 const SYMBOLS: &str = "§±!@#$%^&*()_+=-[]{}|/?.>,<;:";
 
-fn generate_characters(string: &str, percent: f32) -> String {
+fn generate_characters(string: &str, len: f32) -> String {
     let mut res = String::new();
     let count = string.chars().count();
-    for _ in 0..(percent * (count as f32)) as i32 {
+    for _ in 0..len as i32 {
         let rng = random::<usize>();
         let index= rng % count;
         res.push(string.chars().nth(index).unwrap());
     }
     res
+}
+
+fn shuffle(string: String) -> String {
+    let mut shuffed_vec = string.chars().collect::<Vec<char>>();
+    
+    shuffed_vec.sort_by(|_,_| {
+        match random::<usize>() % 2 {
+            0 => Ordering::Less,
+            1 => Ordering::Greater,
+            _ => Ordering::Equal
+        }
+    });
+
+    shuffed_vec.into_iter().collect()
 }
 
 fn main() {
@@ -21,7 +36,6 @@ fn main() {
     let matches = App::from(yaml).get_matches();
 
     let password_length = matches.value_of("length").unwrap_or("20").parse().unwrap();
-
     let numbers_percent: f32 = matches.value_of("numbers").unwrap_or("0.25").parse().unwrap();
     let symbols_percent: f32 = matches.value_of("symbols").unwrap_or("0.25").parse().unwrap();
     let letters_percent: f32 = matches.value_of("letters").unwrap_or("0.5").parse().unwrap();
@@ -31,10 +45,9 @@ fn main() {
     }
 
     let mut password = String::with_capacity(password_length);
-    password += &generate_characters(NUMBERS, numbers_percent);
-    password += &generate_characters(LETTERS, letters_percent);
-    password += &generate_characters(SYMBOLS, symbols_percent);
+    password += &generate_characters(NUMBERS, numbers_percent * password_length as f32);
+    password += &generate_characters(LETTERS, letters_percent * password_length as f32);
+    password += &generate_characters(SYMBOLS, symbols_percent * password_length as f32);
 
-
-    println!("{}", password);
+    println!("{}", shuffle(password));
 }
